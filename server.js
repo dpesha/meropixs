cb5f1a576d56c93c32210babcbf60015b0cc55cf
libra guest <dipesh.acharya@gmail.com>
2012-07-07 02:51:22 -0400

    Second commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Hello Meropixs\n");
 });
 
 var port = process.env.PORT || 3000;
